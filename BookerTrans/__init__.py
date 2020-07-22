d9e53740ca99313bedd41c158ed2e64fc54bd470
wizardforcel <562826179@qq.com>
2020-07-22 22:27:53 +0800

    将 BS4 改为 PyQuery 修复列表翻译问题

--- BookerTrans/__init__.py
+++ BookerTrans/__init__.py
@@ -3,26 +3,20 @@
 import re
 import time
 from os import path
-from bs4 import BeautifulSoup as bs
+from pyquery import PyQuery as pq
 from .api import GoTransApi
 from . import config
 
 __author__ = "ApacheCN"
 __email__ = "apachecn@163.com"
 __license__ = "SATA"
-__version__ = "2020.04.29.1"
+__version__ = "2020.07.22"
 
 RE_CODE = r'<(pre|code)[^>]*?>[\s\S]*?</\1>'
 RE_TAG = r'<[^>]*?>'
 RE_ENTITY = r'&(\w+|#x?\d+);'
 
 api = GoTransApi()
-
-def set_inner_html(elem, html):
-    body = bs('<body>' + html + '</body>', 'lxml').body
-    elem.clear()
-    for ch in list(body.children):
-        elem.append(ch)
 
 def tags_preprocess(html):
 
@@ -103,58 +97,63 @@ def trans_one(html):
 
 def trans_html(html):
     html = process_code(html)
-    root = bs(html, 'lxml')
+    root = pq(html)
     
     # 处理 <p> <h?>
-    elems = root.select('p, h1, h2, h3, h4, h5, h6')
+    elems = root('p, h1, h2, h3, h4, h5, h6')
     for elem in elems:
-        to_trans = elem.decode_contents()
+        elem = pq(elem)
+        to_trans = elem.html()
         trans = trans_one(to_trans)
         if not trans: continue
-        set_inner_html(elem, trans)
+        elem.html(trans)
         
     # 处理 <blockquote> <td> <th>
-    elems = root.select('blockquote, td, th')
+    elems = root('blockquote, td, th')
     for elem in elems:
-        if elem.p: continue
-        to_trans = elem.decode_contents()
+        elem = pq(elem)
+        if elem.children('p'): continue
+        to_trans = elem.html()
         trans = trans_one(to_trans)
         if not trans: continue
-        set_inner_html(elem, trans)
+        elem.html(trans)
     
     # 处理 <li>
-    elems = root.select('li')
+    elems = root('li')
     for elem in elems:
-        if elem.p: continue
+        elem = pq(elem)
+        if elem.children('p'): continue
         
         # 如果有子列表，就取下来
         sub_list = None
-        if elem.ul: sub_list = elem.ul
-        if elem.ol: sub_list = elem.ol
-        if sub_list: sub_list.extract()
+        if elem.children('ul'): sub_list = elem.children('ul')
+        if elem.children('ol'): sub_list = elem.children('ol')
+        if sub_list: sub_list.remove()
         
-        to_trans = elem.decode_contents()
+        to_trans = elem.html()
         trans = trans_one(to_trans)
         if not trans: continue
-        set_inner_html(elem, trans)
+        elem.html(trans)
         
         # 将子列表还原
         if sub_list: elem.append(sub_list)
     
-    return root.decode()
+    return str(root)
 
 def process_code(html):
-    root = bs(html, 'lxml')
+    root = pq(html)
     
-    pres = root.select('div.code, div.Code')
+    pres = root('div.code, div.Code')
     for p in pres:
-        newp = bs('<pre></pre>', 'lxml').pre
-        newp.append(p.text)
+        p = pq(p)
+        newp = pq('<pre></pre>')
+        newp.append(p.text())
         p.replace_with(newp)
         
-    codes = root.select('span.inline-code, span.CodeInline')
+    codes = root('span.inline-code, span.CodeInline')
     for c in codes:
-        newc = bs('<code></code>', 'lxml').code
+        c = pq(c)
+        newc = pq('<code></code>')
         newc.append(c.text)
         c.replace_with(newc)
         
