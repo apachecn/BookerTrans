7f59464d093bf0147b16bf724aebf53a032b2b86
wizardforcel <562826179@qq.com>
2021-04-08 23:21:38 +0800

    2021-04-08 23:21:38

--- BookerTrans/apis/GoTransSeleniumApi.py
+++ BookerTrans/apis/GoTransSeleniumApi.py
@@ -2,34 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import sys
+from .SeleniumApi import SeleniumApi
 
-class GoTransSeleniumApi:
+class GoTransSeleniumApi(SeleniumApi):
 
-    def __init__(self):
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--log-level=3')
-        self._driver = webdriver.Chrome(options=options)
-        # 打开谷歌翻译
-        self._driver.get('https://translate.google.cn/?sl=auto&tl=zh-CN')
-        self._lang = ('auto', 'zh-CN')
-
-    def translate(self, s, src='auto', dst='zh-CN'):
-        if self._lang != (src, dst):
-            self._driver.get(f'https://translate.google.cn/?sl={src}&tl={dst}')
-            self._lang = (src, dst)
-        # 获取文本输入域
-        text_dummy = self._driver.find_element_by_css_selector('textarea')
-        text_dummy.clear()
-        text_dummy.send_keys(s)
-        time.sleep(1)
-        text_translation = self._driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]/span[1]')
-        # 返回翻译结果
-        return text_translation.text
-
-    def __del__(self):
-        self._driver.close()
+    def get_settings(self):
+        return {
+            'default_url': 'https://translate.google.cn/?sl=auto&tl=zh-CN',
+            'url_temp': 'https://translate.google.cn/?sl={src}&tl={dst}',
+            'src_text': 'textarea',
+            'dst_text': 'html>body>c-wiz>div>div:nth-of-type(2)>c-wiz>div:nth-of-type(2)>c-wiz>div:nth-of-type(1)>div:nth-of-type(2)>div:nth-of-type(2)>c-wiz:nth-of-type(2)>div:nth-of-type(5)>div>div:nth-of-type(1)>span:nth-of-type(1)',
+        }
 
 def main():
     api = GoTransSeleniumApi()
