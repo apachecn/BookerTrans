865be638584401d25fabc9a8a72a65b3bdb1ff2f
wizardforcel <562826179@qq.com>
2021-04-08 23:17:58 +0800

    2021-04-08 23:17:58

--- BookerTrans/apis/BaiTransSeleniumApi.py
+++ BookerTrans/apis/BaiTransSeleniumApi.py
@@ -2,31 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import sys
+from .SeleniumApi import SeleniumApi
 
-class BaiTransSeleniumApi:
+class BaiTransSeleniumApi(SeleniumApi):
 
-    def __init__(self):
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--log-level=3')
-        self._driver = webdriver.Chrome(options=options)
-        self._driver.get('https://fanyi.baidu.com/#auto/zh-CN')
-        self._lang = ('auto', 'zh-CN')
-
-    def translate(self, s, src='auto', dst='zh-CN'):
-        if self._lang != (src, dst):
-            self._driver.get(f'https://fanyi.baidu.com/#{src}/{dst}')
-            self._lang = (src, dst)
-        text_dummy = self._driver.find_element_by_css_selector('textarea.textarea')
-        text_dummy.clear()
-        text_dummy.send_keys(s)
-        time.sleep(1)
-        text_translation = self._driver.find_element_by_css_selector('p.target-output')
-        return text_translation.text
-
-    def __del__(self):
-        self._driver.close()
+    def get_settings(self):
+        return {
+            'default_url': 'https://fanyi.baidu.com/#auto/zh-CN',
+            'url_temp': 'https://fanyi.baidu.com/#{src}/{dst}',
+            'src_text': 'textarea.textarea',
+            'dst_text': 'p.target-output',
+        }
 
 def main():
     api = BaiTransSeleniumApi()
---
+++ BookerTrans/apis/SeleniumApi.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+import sys
+
+class SeleniumApi:
+
+    def get_settings(self):
+        return {
+            'default_url': '',
+            'url_temp': '',
+            'src_text': '',
+            'dst_text': '',
+        }
+
+    def __init__(self):
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--log-level=3')
+        self._driver = webdriver.Chrome(options=options)
+        self._driver.get(self.get_settings()['default_url'])
+        self._lang = ('auto', 'zh-CN')
+
+    def translate(self, s, src='auto', dst='zh-CN'):
+        settings = self.get_settings()
+        if self._lang != (src, dst):
+            self._driver.get(settings['url_temp']
+                .replace('{src}', src)
+                .replace('{dst}', dst))
+            self._lang = (src, dst)
+        text_dummy = self._driver \
+            .find_element_by_css_selector(settings['src_text'])
+        text_dummy.clear()
+        text_dummy.send_keys(s)
+        time.sleep(1)
+        text_translation = self._driver \
+            .find_element_by_css_selector(settings['dst_text'])
+        return text_translation.text
+
+    def __del__(self):
+        self._driver.close()
