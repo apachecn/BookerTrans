b66fde2e4e93d4375031b09b41d1bb339115156f
wizardforcel <562826179@qq.com>
2021-04-09 22:27:45 +0800

    2021-04-09 22:27:45

--- BookerTrans/__init__.py
+++ BookerTrans/__init__.py
@@ -10,7 +10,7 @@ from . import config
 __author__ = "ApacheCN"
 __email__ = "apachecn@163.com"
 __license__ = "SATA"
-__version__ = "2020.12.01"
+__version__ = "2021.4.9.0"
 
 RE_CODE = r'<(pre|code|tt|var|kbd)[^>]*?>[\s\S]*?</\1>'
 RE_TAG = r'<[^>]*?>'
