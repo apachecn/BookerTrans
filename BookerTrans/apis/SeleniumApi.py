652b1e195514c2102fbf372b867c81ba92c93555
wizardforcel <562826179@qq.com>
2021-11-17 11:32:44 +0800

    2021-11-17 11:32:44

--- BookerTrans/apis/SeleniumApi.py
+++ BookerTrans/apis/SeleniumApi.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
 import time
 import sys
+import re
 
 class SeleniumApi:
 
@@ -10,34 +12,46 @@ class SeleniumApi:
             'url_temp': '',
             'src_text': '',
             'dst_text': '',
+            'wait_sec': 10000000,
+            'check_sec': 0.1,
         }
 
+    def load_page(self, src='auto', dst='zh-CN'):
+        settings = self.get_settings()
+        self._driver.get(settings['url_temp']
+            .replace('{src}', src)
+            .replace('{dst}', dst))
+        self._driver.implicitly_wait(settings['wait_sec'])
+        self._lang = (src, dst)
+        
     def __init__(self):
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_argument('--log-level=3')
         self._driver = webdriver.Chrome(options=options)
-        self._driver.get(self.get_settings()['url_temp']
-            .replace('{src}', 'auto')
-            .replace('{dst}', 'zh-CN'))
-        self._lang = ('auto', 'zh-CN')
+        self.load_page('auto', 'zh-CN')
 
     def translate(self, s, src='auto', dst='zh-CN'):
+        if re.search(r'^\s*$', s): return ""
         settings = self.get_settings()
         if self._lang != (src, dst):
-            self._driver.get(settings['url_temp']
-                .replace('{src}', src)
-                .replace('{dst}', dst))
-            self._lang = (src, dst)
-        text_dummy = self._driver \
+            self.load_page(src, dst)
+        el_src = self._driver \
             .find_element_by_css_selector(settings['src_text'])
-        text_dummy.clear()
-        text_dummy.send_keys(s)
-        time.sleep(1)
-        text_translation = self._driver \
+        el_src.clear()
+        def wait_clear_callback(dvr):
+            el_dst = dvr.find_element_by_css_selector(settings['dst_text'])
+            return el_dst is None
+        WebDriverWait(driver, settings['wait_sec']).until(wait_clear_callback)
+        el_src.send_keys(s)
+        def wait_trans_callback(dvr):
+            el_dst = dvr.find_element_by_css_selector(settings['dst_text'])
+            return el_dst is not None and el_dst.text != ""
+        WebDriverWait(driver, settings['wait_sec']).until(wait_trans_callback)
+        el_dst = self._driver \
             .find_element_by_css_selector(settings['dst_text'])
-        return text_translation.text
+        return el_dst.text
 
     def __del__(self):
         self._driver.close()
