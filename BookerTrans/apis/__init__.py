ca516892c70f57102fcde5ef4f3c959e2347a92b
wizardforcel <562826179@qq.com>
2021-12-02 17:34:49 +0800

    2021-12-02 17:34:49

--- BookerTrans/apis/__init__.py
+++ BookerTrans/apis/__init__.py
@@ -2,14 +2,12 @@ from .GoTransSeleniumApi import GoTransSeleniumApi
 from .BaiTransSeleniumApi import BaiTransSeleniumApi
 from .SoTransSeleniumApi import SoTransSeleniumApi
 from .YoudaoTransSeleniumApi import YoudaoTransSeleniumApi
-from .BaiTransApi import BaiTransApi
 from .YoudaoTransApi import YoudaoTransApi
 from .QTransSeleniumApi import QTransSeleniumApi
 from .DeeplTransSeleniumApi import DeeplTransSeleniumApi
 
 apis = {
     'google_selenium': GoTransSeleniumApi,
-    'baidu': BaiTransApi,
     'baidu_selenium': BaiTransSeleniumApi,
     'sogou_selenium': SoTransSeleniumApi,
     'youdao': YoudaoTransApi,
