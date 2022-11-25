7f674b985b3bedc667096fbccb71d4ff019a8f5a
wizardforcel <562826179@qq.com>
2022-11-25 21:44:43 +0800

    2022-11-25 21:44:42

--- BookerTrans/__main__.py
+++ BookerTrans/__main__.py
@@ -3,7 +3,7 @@
 import os
 from os import path
 from argparse import ArgumentParser
-import threding
+import threading
 import traceback
 from concurrent.futures import ThreadPoolExecutor
 from . import trans_html, __version__
