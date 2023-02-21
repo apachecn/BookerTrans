4c62c9e3f20ae0d0acae03413b553aa0e5daa67b
wizardforcel <562826179@qq.com>
2023-02-21 12:49:37 +0800

    2023-02-21 12:49:37

--- BookerTrans/util.py
+++ BookerTrans/util.py
@@ -8,6 +8,8 @@ def safe(default=None):
     def wrapper(func):
         def inner(*args, **kw):
             try: return func(*args, **kw)
-            except: traceback.print_exc()
+            except: 
+                traceback.print_exc()
+                return default
         return inner
     return wrapper
